27ba1ecc780573542e06c6caa780f494df5f29a8
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-28 23:37:17 -0400

    Added javascript code initializing score variable and onclick click() function

---
+++ klicker.js
@@ -0,0 +1,9 @@
+var score = 0; // initializing score variable
+
+
+function click() {
+  // onclick function - will add 1 to score, and for now, to test, will console.log("Click " + score + "!")
+  score += 1;
+  // for testing purposes
+  console.log("Click " + score + "!");
+}
