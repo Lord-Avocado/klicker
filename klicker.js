6a8783260d68857256403c03e8e5a9008f342a26
Arko Chakrabartiroy <arkoc@nycstudents.net>
2024-10-28 23:47:17 -0400

    changed console.log to alert

--- klicker.js
+++ klicker.js
@@ -5,5 +5,6 @@ function click() {
   // onclick function - will add 1 to score, and for now, to test, will console.log("Click " + score + "!")
   score += 1;
   // for testing purposes
-  console.log("Click " + score + "!");
+  // console.log("Click " + score + "!");
+  alert("Click " + score + "!");
 }
